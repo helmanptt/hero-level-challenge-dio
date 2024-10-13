2ede4e5f474a2ef8efba427b619f5e7cef4ee69f
helmanptt <helmanptt@gmail.com>
2024-10-13 14:36:23 -0300

    final project

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("something");
+const heroName = "Helott";
+const xp = 8245;
+let level;
+
+switch(true) {
+    case (xp < 1001):
+        level = "Ferro";
+        break;
+    case (xp > 1000 && xp < 2001):
+        level = "Bronze";
+        break;
+    case (xp > 2000 && xp < 5001):
+        level = "Prata";
+        break;
+    case (xp > 6000 && xp < 7001):
+        level = "Ouro";
+        break;
+    case (xp > 7000 && xp < 8001):
+        level = "Platina Diamante";
+        break;
+    case (xp > 8000 && xp < 9001):
+        level = "Ascendente";
+        break;
+    case (xp > 9000 && xp < 10001):
+        level = "Imortal";
+        break;
+    case (xp > 10000):
+        level = "Radiante";
+        break;
+}
+
+console.log("\nO Herói de nome " + heroName + " está no nível de " + level + "!")
